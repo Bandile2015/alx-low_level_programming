b2f711908312707491e9fde853f239d5d959df8e
sindisiwe <sindisiwemashinini9@gmail.com>
2022-09-08 10:07:48 -0700

    coding

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 
 /**
-* main -print the string in the function
-*
-* Description: using the main function
-* this program prints "Programming is like building a multilingual puzzle
-* Return: 0
+* main - print the string in the function
+* 
+* Return: Always 0 (Programming is like building a multilingual puzzle)
 */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0),
+	return (0);
 }

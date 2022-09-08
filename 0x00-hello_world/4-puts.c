c8766a58819194a73d202e0564ddd6724401b950
sindisiwe <sindisiwemashinini9@gmail.com>
2022-09-08 08:23:30 -0700

    coding

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,14 @@
-# include <stdio.h>
+#include <studio.h>
 
 /**
-*main - print the string in the function
+* main - print the string in the function
 *
-*Description: using the main function
-*this program prints "programming is like building a multilingual puzzle
-*Return: 0
+* Description: using the main function 
+* this program prints "Programming is like building a multilingual puzzle
+* Return: 0
 */
 int main(void)
-{	puts("\"programming is the like building a multilingual puzzle");
+{
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-
 }

4f9e32cfbb3b6d5e6f3272dda08c17e55d15a1f8
sindisiwe <sindisiwemashinini9@gmail.com>
2022-09-08 15:48:48 -0700

    qoute

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,11 +3,11 @@
 /**
  * main - print the string in the function
  *
- * this program prints "Programming is like building a multilingual puzzle
+ * this program prints "Programming is like building a multilingual puzzle"
  * Return: 1
  */
 int main(void)
 {
-	printf(2,"and that piece of art is useful\"- Dora Korpar, 2015-10-19\n", 59);
+	write(2,"and that piece of art is useful\"- Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }

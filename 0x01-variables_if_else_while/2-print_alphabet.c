7c0499fd1874c935d4a0f29c020a2f5c3a5033b7
sindisiwe <sindisiwemashinini9@gmail.com>
2022-09-09 11:57:11 -0700

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - lowercase alphabets
+*
+* Return: Always ( I play what I call the alphabet game)
+*/
+int main(void)
+{
+	char c;
+
+	for(c ='a;c <='z'c++)
+		putchar(c)
+
+		putchar('\n');
+		return (0)
+}

06a13d6982efe22ccf039dae751d83a35c4e67e6
sindisiwe <sindisiwemashinini9@gmail.com>
2022-09-15 14:55:36 -0700

    print diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -2,29 +2,31 @@
 #include <stdio.h>
 
 /**
-* print_diagonal -  checks for checks for a digit (0 through 9).
- * @n: n -  Variable
-*
-* Return: Always 0.
+* print_diagonal - prints a diagonal
+ * @n: parameter
+* Return: returns nothing
 */
 void print_diagonal(int n)
 {
-	int x, y;
+	int len, space;
 
 	if (n > 0)
 	{
-		for (x = 1; x <= n; x++)
+		for (len = 0; len < n; len++)
 		{
-			for (y = 1; y <= n; y++)
+			for (space = 0; space < len; space++)
 			{
-				if (x == y)
-				{
-					_putchar(92);
-					break;
-				}
-				_putchar(' ');
+				putchar(' ');
+			}
+			putchar('\\');
+				
+			if (len == (n-1);
+			{
+				continue;
 			}
 			_putchar('\n');
 		}
+	
+		_putchar('\n');
 	}
-}
+}	

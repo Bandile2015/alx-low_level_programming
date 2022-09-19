2f0961246ed5e82d5f0a890058537d8834596446
sindisiwe <sindisiwemashinini9@gmail.com>
2022-09-19 13:00:10 -0700

    puts c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,18 +1,19 @@
 #include "main.h"
+#include <unistd.h>
 
 /**
-* _puts - a function that returns the length of a string
-*  @s: char input
-*Return: new line
+* _puts - prints a string, to stdout
+* @str: value to be evaluate.
+* Return: not.
 */
-
 void _puts(char *str);
 {
-	int i = 1, sum = 0;
-	 char *str;
+	int l = 0;
 
-	 str (pl != '\0')
-	 {
-		 _puts(str);
-	 return (0);
+	while (*(str + l) != '\0')
+	{
+		_putchar(str[l]);
+		l++;
+	}
+	_putchar('\n');
 }

5e75ec4b78995b31036638609e21322c0d3eb7a0
sindisiwe <sindisiwemashinini9@gmail.com>
2022-09-21 13:31:42 -0700

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,17 +1,23 @@
 #include "main.h"
 
 /**
-* _strcat - Entry point
-* @s: pointed destination
-* @b: constant byte
-* @n: bytes
-* Return: Always 0 (Success)
+* _strcat - concatenates the string pointed to by @src to
+* the end of the string pointed to by @dest
+* @dest: String that will be appended
+* @src: String to be concatenated upon
+*
+* Return: returns poiner to @dest
 */
+
 char *_strcat(char *dest, char *src);
 {
-	unsigned int i;
+	int index = 0, dest_len = 0;
 
-	for (i = 0; i < n; i++)
-		s[i] = b;
-	return (s);
+	while (dest[index++])
+		dest_len++;
+
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
+
+	return (dest);
 }

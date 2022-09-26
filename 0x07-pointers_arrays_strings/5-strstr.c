745a8e63f9d86cead8300d3a57c76f672dadde79
sindisiwe <sindisiwemashinini9@gmail.com>
2022-09-26 10:21:20 -0700

    strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * _strstr - first occurrence of the substring needle in the string haystack
- *  @haystack: main str to be examined
- *  @needle: searched in haystack
- *  Return: return 0
+ * _strstr - first occurrence of the substring needle in the string haystack 
+ * @haystack: main str to be examined
+ * @needle: searched in haystack
+ * Return: return 0
  */
 
 char  *_strstr(char *haystack, char *needle)

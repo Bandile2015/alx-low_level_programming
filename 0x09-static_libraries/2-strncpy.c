6fc7dbc56262633113ee88af56c713be96fdc7aa
sindisiwe <sindisiwemashinini9@gmail.com>
2022-09-30 13:57:03 -0700

    create static lib

---
+++ 0x09-static_libraries/2-strncpy.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * _strncpy - string copy
+ * @dest: first input character
+ *  @src: second input character
+ *  @n: number of bytes
+ *
+ *  Description: the function copies a string
+ *
